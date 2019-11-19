a5693312a3784370190302a7cfd409cdb1d6f256
wcy <1536022550@qq.com>
2019-11-19 15:33:39 +0800

    add：添加所有专利findAll()功能

--- patent/src/main/java/com/suixingpay/patent/dao/PatentDao.java
+++ patent/src/main/java/com/suixingpay/patent/dao/PatentDao.java
@@ -1,7 +1,11 @@
 package com.suixingpay.patent.dao;
 
+import com.suixingpay.patent.vo.Patent;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PatentDao {
+    @Select("SELECT * FROM patent")
+    public Patent findAll();
 }
