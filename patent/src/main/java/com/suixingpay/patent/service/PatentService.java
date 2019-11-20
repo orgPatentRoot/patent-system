4d2db72d5178775f9077e3fd8afb81b44f880b2c
zhu_js <zhu_js@suixingpay.com>
2019-11-20 12:08:14 +0800

    Merge remote-tracking branch 'origin/zhujinseng'
    # Conflicts:
    #	.idea/vcs.xml

--- patent/src/main/java/com/suixingpay/patent/service/PatentService.java
+++ patent/src/main/java/com/suixingpay/patent/service/PatentService.java
@@ -1,7 +1,9 @@
 package com.suixingpay.patent.service;
 
+import com.suixingpay.patent.pojo.Patent;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface PatentService {
+    public int insertPatentSevice(Patent patent);
 }
--- patent/src/main/java/com/suixingpay/patent/service/impl/PatentServiceImpl.java
+++ patent/src/main/java/com/suixingpay/patent/service/impl/PatentServiceImpl.java
@@ -1,6 +1,16 @@
 package com.suixingpay.patent.service.impl;
 
+import com.suixingpay.patent.mapper.PatentMapper;
+import com.suixingpay.patent.pojo.Patent;
 import com.suixingpay.patent.service.PatentService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PatentServiceImpl implements PatentService {
+    @Autowired
+    private PatentMapper patentMapper;
+
+    @Override
+    public int insertPatentSevice(Patent patent) {
+        return patentMapper.insert(patent);
+    }
 }
